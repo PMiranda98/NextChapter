f79a4e6d73f5226c5a80c53a7e70503e80d01fc9
Pedro Nuno Miranda <n.miranda@inelcis.pt>
2024-06-28 10:13:10 +0100

    solution init

---
+++ src/AuctionService/Controllers/BaseApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuctionService.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BaseApiController : ControllerBase
+{
+  private readonly ILogger<BaseApiController> _logger;
+
+    public BaseApiController(ILogger<BaseApiController> logger)
+    {
+        _logger = logger;
+    }
+}
