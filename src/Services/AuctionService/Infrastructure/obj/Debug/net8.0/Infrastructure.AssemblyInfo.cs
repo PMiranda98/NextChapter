803cad854aac18f5092bf8395e532b547a8d7756
Pedro Nuno Miranda <n.miranda@inelcis.pt>
2024-07-08 16:29:11 +0100

    Gateway service added to the solution. Gateway service configuration and docker support added to the service.

--- src/Services/AuctionService/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
+++ src/Services/AuctionService/Infrastructure/obj/Debug/net8.0/Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d293a6753735201b8cd4263cbabff08cc24e69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120e44ca8033423b0b123786e5727fc866b192ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Services/AuctionService/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ src/Services/AuctionService/Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9d293a6753735201b8cd4263cbabff08cc24e69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+120e44ca8033423b0b123786e5727fc866b192ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
