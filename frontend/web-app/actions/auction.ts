ee3f27513ede82205171730e4bcf93ae3df3e652
Pedro Nuno Miranda <n.miranda@inelcis.pt>
2024-07-18 13:04:50 +0100

    web-app: finishing the form. get, put, post and delete actions addded.

--- frontend/web-app/actions/auction.ts
+++ frontend/web-app/actions/auction.ts
@@ -1,9 +1,8 @@
 'use server'
 
+import { fetchWrapper } from "@/lib/fetchWrapper";
 import { Auction, PagedResults } from "@/types";
 
 export async function getData(queryString: string): Promise<PagedResults<Auction>> {
-  const result = await fetch(`http://localhost:6001/search${queryString}`);
-  if (!result.ok) throw new Error('Failed to fetch data');
-  return result.json();
+  return await fetchWrapper.get(`search${queryString}`)
 }
--- frontend/web-app/components/auctions/AuctionForm.tsx
+++ frontend/web-app/components/auctions/AuctionForm.tsx
@@ -1,12 +1,19 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FieldValues, useForm } from 'react-hook-form'
 import Input from '../core/Input'
 import { Button } from 'flowbite-react'
+import DateInput from '../core/DateInput'
 
 export default function AuctionForm() {
-  const {control, handleSubmit, setFocus, formState: {isSubmitting, isValid, isDirty, errors}} = useForm()
+  const {control, handleSubmit, setFocus, formState: {isSubmitting, isValid, isDirty, errors}} = useForm({
+    mode: 'onTouched'
+  })
+
+  useEffect(() => {
+    setFocus('make')
+  }, [setFocus])
 
   const onSubmit = (data: FieldValues) => {
     console.log(data)
@@ -16,6 +23,18 @@ export default function AuctionForm() {
     <form className='flex flex-col mt-3' onSubmit={handleSubmit(onSubmit)}>
       <Input label='Make' name='make' control={control} rules={{required: 'Make is required.'}} />
       <Input label='Model' name='model' control={control} rules={{required: 'Model is required.'}} />
+      <Input label='Color' name='color' control={control} rules={{required: 'Color is required.'}} />
+      <div className='grid grid-cols-2 gap-3'>
+        <Input label='Year' name='year' control={control} type='number' rules={{required: 'Year is required.'}} />
+        <Input label='Mileage' name='mileage' control={control} type='number' rules={{required: 'Mileage is required.'}} />
+      </div>
+      <Input label='Image URL' name='imageUrl' control={control} rules={{required: 'Image URL is required.'}} />
+      <div className='grid grid-cols-2 gap-3'>
+        <Input label='Reserve Price (enter 0 if no reserve)' name='reservePrice' control={control} type='number' rules={{required: 'Reserve price is required.'}} />
+        <DateInput label='Auction end date/time' name='auctionEnd' control={control} rules={{required: 'Auction end date is required.'}} />
+      </div>
+
+
       <div className='flex justify-between'>
         <Button outline color='gray'>Cancel</Button>
         <Button
