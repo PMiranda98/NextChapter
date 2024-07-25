42b3be2d8e628f9b1952fe56b2c5faa6f5530424
Pedro Nuno Miranda <n.miranda@inelcis.pt>
2024-07-25 09:43:24 +0100

    Offer service: Advertisement deleted consumer added.

---
+++ src/Services/OfferService/Application/Handlers/Advertisements/Delete.cs
@@ -0,0 +1,40 @@
+﻿using Domain.DTOs.Output;
+using Domain.Entities;
+using Domain.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.Advertisements
+{
+    public class Delete
+    {
+        public class Command : IRequest
+        {
+            public string Id { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly IOfferRepository _offerRepository;
+
+            public Handler(IOfferRepository offerRepository)
+            {
+                _offerRepository = offerRepository;
+            }
+
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var offers = await _offerRepository.ListOffersByAdvertisement(request.Id, cancellationToken);
+                foreach (var offer in offers)
+                {
+                    if(offer.Status == OfferStatus.Live) offer.Status = OfferStatus.Rejected;
+                }
+                await _offerRepository.SaveChangesAsync(cancellationToken);
+            }
+        }
+    }
+}
---
+++ src/Services/OfferService/Infrastructure/Consumers/AdvertisementService/AdvertisementDeletedConsumer.cs
@@ -0,0 +1,28 @@
+﻿using Application.Handlers.Advertisements;
+using EventBus.Contracts;
+using MassTransit;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Consumers.AdvertisementService
+{
+    public class AdvertisementDeletedConsumer : IConsumer<AdvertisementDeleted>
+    {
+        private readonly IMediator _mediator;
+
+        public AdvertisementDeletedConsumer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task Consume(ConsumeContext<AdvertisementDeleted> context)
+        {
+            Console.WriteLine("=====> Consuming advertisement deleted: " + context.Message.Id);
+
+            await _mediator.Send(new Delete.Command { Id = context.Message.Id });
+        }
+    }
+}
